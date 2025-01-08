7ef51fefc91fbf29d9e68bc6308b81ffc395866a
Jason Zhang <jason.gp.zhang@gmail.com>
2025-01-08 14:55:20 +0800

    better design for the theme color converter
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

--- ThemeColorConverter.cs
+++ ThemeColorConverter.cs
@@ -12,43 +12,42 @@ namespace NewLeaf
 {
     public class ThemeColorConverter : IValueConverter
     {
+        public string Type { get; set; }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is string color)
             {
-                if (parameter is string component)
+                string componentColor = Type + color;
+                // For a title, it could be the one of the following keys:
+                //     - "TitleYellow"
+                //     - "TitleGreen"
+                //     - "TitlePink"
+                //     - "TitlePurple"
+                //     - "TitleBlue"
+                //     - "TitleGray"
+                //     - "TitleCarbon"
+                //
+                // For a content, it could be the one of the following keys:
+                //     - "ContentYellow"
+                //     - "ContentGreen"
+                //     - "ContentPink"
+                //     - "ContentPurple"
+                //     - "ContentBlue"
+                //     - "ContentGray"
+                //     - "ContentCarbon"
+                //
+                // For a picker, it could be the one of the following keys:
+                //     - "PickerYellow"
+                //     - "PickerGreen"
+                //     - "PickerPink"
+                //     - "PickerPurple"
+                //     - "PickerBlue"
+                //     - "PickerGray"
+                //     - "PickerCarbon"
+                if (Application.Current.Resources.Contains(componentColor))
                 {
-                    string componentColor = component + color;
-                    // For a title, it could be the one of the following keys:
-                    //     - "TitleYellow"
-                    //     - "TitleGreen"
-                    //     - "TitlePink"
-                    //     - "TitlePurple"
-                    //     - "TitleBlue"
-                    //     - "TitleGray"
-                    //     - "TitleCarbon"
-                    //
-                    // For a content, it could be the one of the following keys:
-                    //     - "ContentYellow"
-                    //     - "ContentGreen"
-                    //     - "ContentPink"
-                    //     - "ContentPurple"
-                    //     - "ContentBlue"
-                    //     - "ContentGray"
-                    //     - "ContentCarbon"
-                    //
-                    // For a picker, it could be the one of the following keys:
-                    //     - "PickerYellow"
-                    //     - "PickerGreen"
-                    //     - "PickerPink"
-                    //     - "PickerPurple"
-                    //     - "PickerBlue"
-                    //     - "PickerGray"
-                    //     - "PickerCarbon"
-                    if (Application.Current.Resources.Contains(componentColor))
-                    {
-                        return (Color)Application.Current.Resources[componentColor];
-                    }
+                    return (Color)Application.Current.Resources[componentColor];
                 }
             }
             return null;
