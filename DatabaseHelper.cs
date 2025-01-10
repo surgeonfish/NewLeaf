d4a4f271f2e99b11f4700aec1b774763a493f5f0
Jason Zhang <jason.gp.zhang@gmail.com>
2025-01-10 22:57:40 +0800

    treewide: remove and sort using
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

--- DatabaseHelper.cs
+++ DatabaseHelper.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media;
 
 namespace NewLeaf
 {
