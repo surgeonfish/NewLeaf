d40849476c947c16708fc091b227de940378b7a3
Jason Zhang <jason.gp.zhang@gmail.com>
2025-01-14 23:11:16 +0800

    add LeafModel
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

---
+++ Model/LeafModel.cs
@@ -0,0 +1,12 @@
+﻿namespace NewLeaf.Model
+{
+    public class LeafModel
+    {
+        public DatabaseHelper DatabaseHelper = null;
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Color { get; set; }
+        public string DateCreated { get; set; }
+        public string DateLastUpdated { get; set; }
+    }
+}
