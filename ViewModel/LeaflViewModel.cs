725a51bc5c5ba07c32dc35f37358150c8595bfe2
Jason Zhang <jason.gp.zhang@gmail.com>
2025-01-19 21:09:32 +0800

    add LeaflViewModel
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

---
+++ ViewModel/LeaflViewModel.cs
@@ -0,0 +1,23 @@
+﻿using NewLeaf.Model;
+
+namespace NewLeaf.ViewModel
+{
+    public class LeaflViewModel : ViewModelBase
+    {
+        public LeafModel LeafModel
+        {
+            get { return LeafModelProperty;  }
+            set
+            {
+                LeafModelProperty = value;
+                OnPropertyChanged();
+            }
+        }
+        private LeafModel LeafModelProperty;
+
+        public LeaflViewModel(LeafModel leafModel)
+        {
+            LeafModel = leafModel;
+        }
+    }
+}
