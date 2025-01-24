602def2a89e471c5d313113a600060e4cb2d1a23
Jason Zhang <jason.gp.zhang@gmail.com>
2025-01-24 11:01:19 +0800

    add LeafListView
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

---
+++ LeafListView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace NewLeaf
+{
+    /// <summary>
+    /// Interaction logic for LeafListView.xaml
+    /// </summary>
+    public partial class LeafListView : ListView
+    {
+        public LeafListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
