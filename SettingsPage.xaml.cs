003ba7fa15e210c00625754b534b75a215a3ec32
Jason Zhang <jason.gp.zhang@gmail.com>
2025-02-05 16:26:12 +0800

    add settings page
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

---
+++ SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace NewLeaf
+{
+    /// <summary>
+    /// Interaction logic for SettingsPage.xaml
+    /// </summary>
+    public partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
