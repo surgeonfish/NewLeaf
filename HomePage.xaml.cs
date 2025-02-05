14cf1e631a414506980a25a589f508101140c8a7
Jason Zhang <jason.gp.zhang@gmail.com>
2025-02-05 15:04:37 +0800

    move leaves to home page
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

---
+++ HomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace NewLeaf
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
