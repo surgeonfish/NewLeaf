d864f3fcc5644733184a6b726ccbb5edad0b0d0b
Jason Zhang <jason.gp.zhang@gmail.com>
2025-02-06 21:02:46 +0800

    Controls: add ThemeSelectionConverter
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

---
+++ Controls/ThemeSelectionConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NewLeaf.Controls
+{
+    public class ThemeSelectionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string theme && parameter is string name)
+            {
+                return theme == name;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
+        {
+            if (value is bool isChecked && parameter is string name)
+            {
+                if (isChecked)
+                {
+                    return name;
+                }
+            }
+            return Binding.DoNothing;
+        }
+    }
+}
