279ac4514f623a91a1797288d9520e2b4b48bb1b
Jason Zhang <jason.gp.zhang@gmail.com>
2025-02-06 16:35:53 +0800

    add attached properties for SolidColorBrush
    This patch currently support for one dummy property named
    "Convert".
    
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

---
+++ Controls/SolidColorBrushAttach.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace NewLeaf.Controls
+{
+    public class SolidColorBrushAttach
+    {
+        public static readonly DependencyProperty ConvertProperty =
+            DependencyProperty.RegisterAttached("Convert", typeof(string), typeof(SolidColorBrushAttach), new PropertyMetadata("Default Value"));
+
+        public static string GetConvert(DependencyObject obj)
+        {
+            return (string)obj.GetValue(ConvertProperty);
+        }
+
+        public static void SetConvert(DependencyObject obj, string value)
+        {
+            obj.SetValue(ConvertProperty, value);
+        }
+    }
+}
