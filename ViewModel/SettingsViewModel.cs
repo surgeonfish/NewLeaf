cb325325c153fba192d6ddbf155d992fc9e2ba0b
Jason Zhang <jason.gp.zhang@gmail.com>
2025-02-06 20:58:44 +0800

    ViewModel: add SettingsViewModel
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

---
+++ ViewModel/SettingsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using NewLeaf.Model;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NewLeaf.ViewModel
+{
+    public partial class SettingsViewModel : SettingsModel, INotifyPropertyChanged
+    {
+        public override string Theme
+        {
+            get { return base.Theme; }
+            set
+            {
+                base.Theme = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
