57ffbf3aea43b2cb127614afbbd623856046af7e
Jason Zhang <jason.gp.zhang@gmail.com>
2025-02-06 16:40:51 +0800

    treewide: binding to colors in dynamic resource through "Convert"
    Use the "Convert" property which is bound to the "Color" of MVVM
    and is attached to a SolidColorBrush to set the dynamic resource
    of the color, because the "Binding" and "DynamicResource" can
    not be used at the same time.
    
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

--- ThemeColorConverter.cs
+++ ThemeColorConverter.cs
@@ -6,13 +6,13 @@ using System.Windows.Media;
 
 namespace NewLeaf
 {
-    public class ThemeColorConverter : IValueConverter
+    public class ThemeColorConverter : IMultiValueConverter
     {
         public string Type { get; set; }
 
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is string color)
+            if (value[0] is string color && value[1] is SolidColorBrush brush)
             {
                 string componentColor = Type + color;
                 // For a title, it could be the one of the following keys:
@@ -43,13 +43,17 @@ namespace NewLeaf
                 //     - "PickerCarbon"
                 if (Application.Current.Resources.Contains(componentColor))
                 {
-                    return (Color)Application.Current.Resources[componentColor];
+                    DependencyProperty colorProperty = SolidColorBrush.ColorProperty;
+                    DynamicResourceExtension dynamicResourceExtension = new DynamicResourceExtension(componentColor);
+                    var resourceReferenceExpression = dynamicResourceExtension.ProvideValue(null);
+                    brush.SetValue(colorProperty, resourceReferenceExpression);
+                    return color;
                 }
             }
             return null;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
