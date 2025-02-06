47b6f9176b8df7fb8cf745c30bd2ca069be798a5
Jason Zhang <jason.gp.zhang@gmail.com>
2025-02-06 22:05:06 +0800

    move ThemeColorConverter to Controls
    Signed-off-by: Jason Zhang <jason.gp.zhang@gmail.com>

--- Controls/ThemeColorConverter.cs
+++ Controls/ThemeColorConverter.cs
@@ -4,7 +4,7 @@ using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
 
-namespace NewLeaf
+namespace NewLeaf.Controls
 {
     public class ThemeColorConverter : IMultiValueConverter
     {
